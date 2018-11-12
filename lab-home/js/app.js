fe038a4909d7296c191d27f7c537f5a3fc0f9541
Sean Swanson <sean-swan@hotmail.com>
2018-11-11 21:55:45 -0800

    Added skydome to donuts, fixed perspective, prettied UI.

--- lab-home/js/app.js
+++ lab-home/js/app.js
@@ -1,33 +1,26 @@
-(function() {
-  let selectedProject = "";
+  let selectedProject = '';
   let projects = [];
-  let projectLinks = document.querySelectorAll(".left-experiment-list-item");
-  let canvasContainer = document.querySelector("#main-canvas-container");
+  let projectLinks = document.querySelectorAll('.left-experiment-list-item');
+  let canvasContainer = document.querySelector('#main-canvas-container');
 
-  let rightViewport = document.querySelector(".right");
+  let rightViewport = document.querySelector('.right');
   let viewportSize = getViewportSize();
 
-  let rightIntro = document.querySelector(".intro");
-  let rightCornerTitle = document.querySelector(".right-corner-title");
-  let rightCornerGitHubLink = document.querySelector(".right-corner-github");
+  let rightIntro = document.querySelector('.intro');
+  let rightCornerTitle = document.querySelector('.right-corner-title');
+  let rightCornerGitHubLink = document.querySelector('.right-corner-github');
 
   let scene, camera, renderer;
 
-  let donuts = [];
-  let torus;
-  let floor;
   let controls;
-  let startDonutAnimation = false;
-  let spinFactor = 0.03;
-  let fallFactor = 0.05;
 
   //----------
   function initPage() {
     projectLinks.forEach(function(project) {
-      project.isSelected = toArray(project.classList).indexOf("selected");
+      project.isSelected = toArray(project.classList).indexOf('selected');
       project.id = project.dataset.id;
       projects.push(project);
-      project.addEventListener("click", function(event) {
+      project.addEventListener('click', function(event) {
         handleProjectSelection(event, project.id);
       });
     });
@@ -38,127 +31,50 @@
   //----------
   function handleProjectSelection(event, projectId) {
     projectLinks.forEach(function(project) {
-      project.classList.remove("selected");
+      project.classList.remove('selected');
     });
 
     selectedProject = event.currentTarget.dataset.id;
     rightCornerTitle.innerText = selectedProject;
-    event.currentTarget.classList.add("selected");
+    event.currentTarget.classList.add('selected');
 
-    if (projectId !== "Intro") {
-      rightIntro.classList.add("hidden");
-      canvasContainer.classList.remove("hidden");
-      window.addEventListener("resize", resizeCanvas, false);
+    if (projectId !== 'Intro') {
+      rightIntro.classList.add('hidden');
+      canvasContainer.classList.remove('hidden');
+      window.addEventListener('resize', resizeCanvas, false);
     } else {
-      rightIntro.classList.remove("hidden");
-      canvasContainer.classList.add("hidden");
+      rightIntro.classList.remove('hidden');
+      canvasContainer.classList.add('hidden');
     }
 
-    if (projectId === "Donuts") {
+    if (projectId !== 'Donuts') {
+      donutClickPrompt.classList.add('hidden');
+    }
+
+    if (projectId === 'Donuts') {
       changeGitHubHref(projectId);
       initDonuts();
       mainDonutLoop();
-    } else if (projectId === "Saturn") {
+    } else if (projectId === 'Saturn') {
       changeGitHubHref(projectId);
-    } else if (projectId === "Thing 1") {
+    } else if (projectId === 'Thing 1') {
       changeGitHubHref(projectId);
-    } else if (projectId === "Thing 2") {
+    } else if (projectId === 'Thing 2') {
       changeGitHubHref(projectId);
     }
   }
 
   //----------
   function cleanUpScene() {
-    scene.traverse( function ( object ) {
-
-    	if ( object.geometry ) object.geometry.dispose();
-    	if ( object.material ) {
-    		if (object.material.map) object.material.map.dispose();
-    		object.material.dispose();
-    	}
-    	scene.remove(object);
-    } );
-  }
-
-  //----------
-  let initDonuts = () => {
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xababab);
-
-    camera = new THREE.PerspectiveCamera(
-      30,
-      (window.innerWidth - 296) / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.set(0, 5, 100);
-
-    renderer = new THREE.WebGLRenderer({ canvas: canvasContainer });
-    renderer.setSize(viewportSize.width, viewportSize.height);
-
-    console.log(scene, camera, renderer);
-    // controls = new THREE.OrbitControls(camera);
-    // camera.position.set( 0, 20, 100 );
-    // controls.update();
-    //
-    // createFloor();
-    // handleClickPromptClick();
-    // handleDonutClick();
-    // Attach the canvasContainer to the DOM.
-  };
-
-  //----------
-  let mainDonutLoop = () => {
-    // if (startDonutAnimation) {
-    let x = throttleDonuts();
-
-    if (x) {
-      createDonuts();
-    }
-
-    donuts.forEach((donut, index) => {
-      donut.donut.position.y -= 1 * fallFactor;
-
-      if (donut.rotateCW) {
-        donut.donut.rotation.y -= 1 * spinFactor;
-      } else {
-        donut.donut.rotation.y += 1 * spinFactor;
+    scene.traverse(function(object) {
+      if (object.geometry) object.geometry.dispose();
+      if (object.material) {
+        if (object.material.map) object.material.map.dispose();
+        object.material.dispose();
       }
-
-      if (donut.donut.position.y <= -20) {
-        scene.remove(donut);
-        donut.donut.geometry.dispose();
-        donut.donut.material.dispose();
-      }
+      scene.remove(object);
     });
-
-    // }
-
-    renderer.render(scene, camera);
-    requestAnimationFrame(mainDonutLoop);
-  };
-
-  //----------
-  let createDonuts = () => {
-    // Geometry takes in width, height, depth values, depending on the shape.
-    let geometry = new THREE.TorusGeometry(randomInRange(1.7, 2.5), 1, 30, 50);
-    // Material takes in an object to configure the geometry's appearance.
-    let material = new THREE.MeshBasicMaterial({ color: randomColor() });
-
-    // Assign a new Mesh object to the shape variable and pass in geometry and material.
-    torus = new THREE.Mesh(geometry, material);
-    torus.position.z = randomInRange(-30, 10);
-    torus.position.x = randomInRange(-15, 15);
-    torus.position.y = randomInRange(18, 24);
-    torus.rotation.y += randomInRange(-5, 5);
-
-    // Add it to the scene.
-    scene.add(torus);
-    donuts.push({ donut: torus, rotateCW: Math.random() < 0.5 });
-  };
-
-  //----------
-  let throttleDonuts = () => Math.random() < 0.03;
+  }
 
   //----------
   let randomColor = () => Math.random() * 0xff0000;
@@ -172,20 +88,20 @@
   //----------
   function changeGitHubHref(projectId) {
     switch (projectId) {
-      case "Intro":
-        rightCornerGitHubLink.href = "http://www.github.com/seanthaswan";
+      case 'Intro':
+        rightCornerGitHubLink.href = 'http://www.github.com/seanthaswan';
         break;
-      case "Donuts":
-        rightCornerGitHubLink.href = "http://www.github.com/seanthaswan";
+      case 'Donuts':
+        rightCornerGitHubLink.href = 'http://www.github.com/seanthaswan';
         break;
-      case "Saturn":
-        rightCornerGitHubLink.href = "http://www.github.com/seanthaswan";
+      case 'Saturn':
+        rightCornerGitHubLink.href = 'http://www.github.com/seanthaswan';
         break;
-      case "Thing 1":
-        rightCornerGitHubLink.href = "http://www.github.com/seanthaswan";
+      case 'Thing 1':
+        rightCornerGitHubLink.href = 'http://www.github.com/seanthaswan';
         break;
-      case "Thing 2":
-        rightCornerGitHubLink.href = "http://www.github.com/seanthaswan";
+      case 'Thing 2':
+        rightCornerGitHubLink.href = 'http://www.github.com/seanthaswan';
         break;
     }
   }
@@ -216,4 +132,3 @@
   }
 
   initPage();
-})();
---
+++ lab-home/js/donuts.js
@@ -0,0 +1,157 @@
+let donutClickPrompt = document.querySelector('.donut-click-prompt');
+let startDonutAnimation;
+let donutSpinFactor = 0.03;
+let donutFallFactor = 0.09;
+let donuts = [];
+let donutFloor;
+let donutTorus;
+
+//----------
+let initDonuts = () => {
+  startDonutAnimation = false;
+  donutClickPrompt.classList.remove('hidden');
+  donuts = [];
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x87cefa);
+  scene.fog = new THREE.Fog(0x87cefa, 0.005, 600);
+
+  camera = new THREE.PerspectiveCamera(
+    45,
+    (window.innerWidth - 296) / window.innerHeight,
+    1,
+    1000
+  );
+  renderer = new THREE.WebGLRenderer({ canvas: canvasContainer });
+  renderer.setSize(viewportSize.width, viewportSize.height);
+
+  console.log(scene, camera, renderer);
+  controls = new THREE.OrbitControls(camera);
+  controls.enablePan = false;
+  // How far you can orbit vertically, upper and lower limits.
+  // Range is 0 to Math.PI radians.
+  controls.minPolarAngle = 0; // radians
+  controls.maxPolarAngle = Math.PI / 2; // radians
+
+  // How far you can orbit horizontally, upper and lower limits.
+  // If set, must be a sub-interval of the interval [ - Math.PI, Math.PI ].
+  controls.minAzimuthAngle = -Infinity; // radians
+  controls.maxAzimuthAngle = Infinity; // radians
+
+  camera.position.set(0, 8.5, 60);
+  console.log(camera.rotation);
+  controls.update();
+
+  createDonutSkyDome();
+  createDonutFloor();
+  handleDonutPromptClick();
+};
+
+//----------
+let mainDonutLoop = () => {
+  if (startDonutAnimation) {
+    let x = throttleDonuts();
+
+    if (x) {
+      createDonuts();
+    }
+
+    donuts.forEach((donut, index) => {
+      donut.donut.position.y -= 1 * donutFallFactor;
+
+      if (donut.rotateCW) {
+        donut.donut.rotation.y -= 1 * donutSpinFactor;
+      } else {
+        donut.donut.rotation.y += 1 * donutSpinFactor;
+      }
+
+      if (donut.donut.position.y <= -20) {
+        scene.remove(donut.donut);
+        donut.donut.geometry.dispose();
+        donut.donut.material.dispose();
+      }
+    });
+  }
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(mainDonutLoop);
+};
+
+//----------
+let createDonutSkyDome = () => {
+  let geometry = new THREE.SphereGeometry(400, 60, 40);
+
+  let uniforms = {
+    texture: {
+      type: 't',
+      value: new THREE.TextureLoader().load('../img/textures/sky.jpg')
+    }
+  };
+
+  let material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: document.getElementById('sky-vertex').textContent,
+    fragmentShader: document.getElementById('sky-fragment').textContent
+  });
+
+  let skybox = new THREE.Mesh(geometry, material);
+  skybox.material.side = THREE.BackSide;
+  skybox.scale.set(1, -1, 1);
+  skybox.rotation.order = 'XYZ';
+  skybox.renderDepth = 1000.0;
+  scene.add(skybox);
+};
+
+//----------
+let createDonuts = () => {
+  // Geometry takes in width, height, depth values, depending on the shape.
+  let geometry = new THREE.TorusGeometry(randomInRange(1.7, 2.5), 1, 30, 50);
+  // Material takes in an object to configure the geometry's appearance.
+  let material = new THREE.MeshBasicMaterial({ color: randomColor() });
+
+  // Assign a new Mesh object to the shape variable and pass in geometry and material.
+  donutTorus = new THREE.Mesh(geometry, material);
+  donutTorus.position.set(
+    randomInRange(-90, 90),
+    randomInRange(30, 35),
+    randomInRange(-90, 90)
+  );
+  
+  donutTorus.rotation.y += randomInRange(-2, 1);
+
+  // Add it to the scene.
+  scene.add(donutTorus);
+  donuts.push({ donut: donutTorus, rotateCW: Math.random() < 0.5 });
+};
+
+//----------
+let createDonutFloor = () => {
+  var texture = new THREE.TextureLoader().load(
+    '../img/textures/grass.jpg',
+    function(texture) {
+      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      texture.offset.set(0, 0);
+      texture.repeat.set(4, 4);
+    }
+  );
+  let geometry = new THREE.PlaneGeometry(300, 300);
+  let material = new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.DoubleSide
+  });
+  let donutFloor = new THREE.Mesh(geometry, material);
+  scene.add(donutFloor);
+  donutFloor.position.set(0, -10, 0);
+  donutFloor.rotation.set(Math.PI / 2, 0, 0);
+};
+
+//----------
+handleDonutPromptClick = event => {
+  donutClickPrompt.addEventListener('click', function(event) {
+    startDonutAnimation = true;
+    donutClickPrompt.classList.add('hidden');
+  });
+};
+
+//----------
+let throttleDonuts = () => Math.random() < 0.1;
